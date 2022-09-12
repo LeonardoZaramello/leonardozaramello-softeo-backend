f366bdae9d65cf5160eaa83dd14b146b14f3ea47
Leonardo Zaramello <leeurw@hotmail.com>
2022-09-12 05:10:53 -0300

    Fix: Mongodb Atlass URI

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ class App {
 
   private database () {
     dotenv.config()
-    mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/Softeobd')
+    mongoose.connect(process.env.MONGODB_URI || 'http://localhost:27017/Softeobd')
   }
 
   private routes () {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app'
 import dotenv from 'dotenv'
 dotenv.config()
-const PORT = process.env.PORT || 3003
+
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => console.log(`Server initialized in port: ${PORT}`))
