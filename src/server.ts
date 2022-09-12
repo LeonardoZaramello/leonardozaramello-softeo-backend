6322b396970b1e3e19575dea381a47f66c10d94a
Leonardo Zaramello <leeurw@hotmail.com>
2022-09-12 02:55:38 -0300

    Fix: heroku deploy

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
+import express from 'express'
 import app from './app'
 
 const port = process.env.PORT || 3001
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
 
 app.listen(port, () => console.log(`Server initialized in port: ${port}`))
