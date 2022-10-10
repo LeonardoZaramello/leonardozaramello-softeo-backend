e327b6ae222e668f9fb5b7bf1b684150b1682597
Leonardo Zaramello <leeurw@hotmail.com>
2022-10-09 22:47:14 -0300

    Fix: PORT for heroku

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const PORT = 3001 || process.env.PORT
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => console.log(`Server initialized in port: ${PORT}`))
