e382627526fca428c2e2d94f43cfa799ae0b0ae5
Leonardo Zaramello <leeurw@hotmail.com>
2022-10-09 22:50:35 -0300

    Fix: mongoose connect with heroku

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ class App {
 
   private database () {
     dotenv.config()
-    mongoose.connect('mongodb://localhost:27017/Softeobd' || process.env.MONGODB_URI)
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Softeobd')
   }
 
   private routes () {
